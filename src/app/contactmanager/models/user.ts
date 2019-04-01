af401c09cae4cae752adcaedbc06c073e495e14c
Pradhan Rishi Sharma <pradhanrishi10@gmail.com>
2019-04-01 14:08:00 +0530

    fix: changes to main-component

--- src/app/contactmanager/models/user.ts
+++ src/app/contactmanager/models/user.ts
@@ -1,11 +1,24 @@
-import { Note } from "./note";
+import {Note} from "./note";
+import {Address} from "./address";
 
 export class User {
-    id: number;
-    birthDate: Date;
-    name: string;
-    avatar: string;
-    bio: string;
+  id: number;
+  birthDate: Date;
+  name: string;
+  avatar: string;
+  bio: string;
+  username: string;
+  email: string;
+  phone: string;
+  address: Address;
+  website: string;
+  company: Company;
 
-    notes: Note[] = [];
+  notes: Note[] = [];
+}
+
+export interface Company {
+  name: string;
+  catchPhrase: string;
+  bs: string;
 }
